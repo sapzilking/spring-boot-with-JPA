53aa598e978ac255418372be1d0d303d80a0e104
sugil <privatesugil@gmail.com>
2021-11-21 16:25:47 +0900

    주문, 주문상품 엔티티 개발 1. 비즈니스 로직 및 조회로직, 생성메서드 추가

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em; //EntityManager는 @Autowired가 아니라 @PersistenceContext를 사용해야 하는데, @Autowired를 가능하게 해서 생성자 주입을 받을 수 있는 이유는 springDataJpa가 해주기 떄문이다.
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+
+
+
+}
